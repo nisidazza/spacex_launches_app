9243c3ed8c5c66b2bffc0020f2679e808ac7fe21
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-12-15 17:07:24 +1300

    display username and job title

--- components/auth-status.tsx
+++ components/auth-status.tsx
@@ -1,18 +1,20 @@
+"use client";
+
 import { Text } from "@chakra-ui/react";
-import { getServerSession } from "next-auth/next";
+import { useSession } from "next-auth/react";
 
 export default async function AuthStatus() {
-  const session = await getServerSession();
+  const { data: session } = await useSession();
   return (
     <div>
-      {session && (
+      {session?.user && (
         <Text
           color={"white"}
           fontWeight={"bold"}
           display={"initial"}
           fontSize="xl"
         >
-          Welcome {session.user?.user_name || null}!
+          Welcome {session.user?.username}, {session.user.job_title}!
         </Text>
       )}
     </div>
