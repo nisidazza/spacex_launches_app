b2c146a0a768f61320d2dd650c951f9f77e7d994
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-12-15 17:09:24 +1300

    remove navabr and add suspense component around launches

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,12 +1,20 @@
 import Launches from "@/components/launches";
-import NavBar from "@/components/navbar";
 import { Flex } from "@chakra-ui/react";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <Flex flexDirection={"column"} minHeight={"100vh"}>
-      <NavBar />
-      <Launches />
+    <Flex
+      flexDirection={"column"}
+      minHeight={"100vh"}
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Suspense
+        fallback={<p className="text-white font-bold text-2xl">Loading...</p>}
+      >
+        <Launches />
+      </Suspense>
     </Flex>
   );
 }
