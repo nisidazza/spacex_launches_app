9cbd0d970f310a6beee4f4cbb11243e7df7578c1
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-12-15 17:09:44 +1300

    remove comment out code

--- components/sign-out.tsx
+++ components/sign-out.tsx
@@ -5,13 +5,6 @@ import { IoLogOutOutline } from "react-icons/io5";
 
 export default function SignOut() {
   return (
-    // <button
-    //   className=" hover:text-stone-200 transition-all font-bold"
-    //   style={{ color: "black" }}
-    //   onClick={() => signOut()}
-    // >
-    //   Sign out
-    // </button>
     <MenuItem icon={<IoLogOutOutline size="20px" />} onClick={() => signOut()}>
       Sign out
     </MenuItem>
