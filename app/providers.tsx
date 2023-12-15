7becc5a2dcddf505612a6c165d97f8474564e85c
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-12-15 17:05:41 +1300

    add sessionProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,15 @@
-// app/providers.tsx
 'use client'
 
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
+import { SessionProvider } from 'next-auth/react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <CacheProvider>
-      <ChakraProvider>{children}</ChakraProvider>
-    </CacheProvider>
-  )
+    <SessionProvider>
+      <CacheProvider>
+        <ChakraProvider>{children}</ChakraProvider>
+      </CacheProvider>
+    </SessionProvider>
+  );
 }
