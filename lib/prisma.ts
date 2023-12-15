3abd0d0fe27c03dcc3b6a6fdd91d5215ccdb261f
Nisida Azzalini <nisida.azzalini@gmail.com>
2023-12-15 17:01:45 +1300

    update prisma setup and schema

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
+//https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices#:~:text=The%20solution%20in%20this%20case,prevent%20instantiating%20extra%20PrismaClient%20instances.
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prisma = global.prisma || new PrismaClient();
+const prisma = globalThis.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV === "development") global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
-export default prisma;
+export const db = prisma;
+
